f418bbe187e01bea26c642856de71c95d33d3a39
Beatriz Bravo <beatriz.bravo-chavez@ten10.com>
2024-01-03 15:25:43 +0000

    added the computer player

---
+++ RockPaperScissors/Player/Computer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace RockPaperScissors
+{
+    public class Computer : IPlayer
+    {
+        private int computerInput;
+
+        public Computer()
+        {
+            computerInput = 0;
+        }
+
+        public int GetComputerInput()
+        {
+            return computerInput;
+        }
+
+        public int PlayerMove()
+        {
+            Random random = new Random();
+            int min = 1;
+            int max = 3;
+            computerInput = random.Next(min, max + 1);
+            return computerInput;
+        }
+    }
+}
